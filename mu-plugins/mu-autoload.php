df9cb8628151a552f6adf3060641af1006018454
R A Van Epps <rave@ravanepps.com>
2021-01-19 14:18:19 -0700

    Update wp graphql gutenberg version

--- mu-plugins/mu-autoload.php
+++ mu-plugins/mu-autoload.php
@@ -3,11 +3,11 @@
  * Autoload classes required by the project.
  *
  * @author Justin Foell <justin.foell@webdevstudios.com>
- * @since  2021-01-07
+ * @since  2021-01-19
  * @package WebDevStudios\MUAutoload
  */
 
-$autoload = ABSPATH . 'wp-content/vendor/autoload.php';
+$autoload = WP_CONTENT_DIR . '/vendor/autoload.php';
 
 if ( is_readable( $autoload ) ) {
 	require_once $autoload;
