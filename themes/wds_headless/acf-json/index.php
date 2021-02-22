2c7e0e7284f20cef07093aee55537c4c5081eebe
Greg Rickaby <gregrickaby@gmail.com>
2021-02-22 10:50:59 -0600

    simplify acf fields

--- themes/wds_headless/acf-json/index.php
+++ themes/wds_headless/acf-json/index.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * The sidebar containing the main widget area.
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package _s
- */
-
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
