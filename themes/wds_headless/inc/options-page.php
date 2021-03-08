9ca3fe9c4d97f1bf8fe506076effbd55d0c40d7f
Tom McFarlin <tom@tommcfarlin.com>
2021-03-08 16:04:17 -0500

    Added support for custom settings page.

--- themes/wds_headless/inc/options-page.php
+++ themes/wds_headless/inc/options-page.php
@@ -7,44 +7,199 @@
  * @since   1.0
  */
 
-add_action( 'admin_menu', 'wds_headless_options_menu' );
+add_action( 'admin_menu', 'wds_headless_options_page' );
 /**
- * Adds the plugin menu item to the 'Tools' menu in the WordPress dashboard.
+ * Adds the plugin menu item to the 'Settings' menu in the WordPress dashboard.
  *
- * @since     1.0.0
+ * @author WebDevStudios
+ * @since  1.0.0
  */
-function wds_headless_options_menu() {
-	add_submenu_page(
-		'tools.php',
+function wds_headless_options_page() {
+	add_options_page(
 		__( 'WDS Headless Theme Settings', 'wds' ),
 		__( 'Theme Settings', 'wds' ),
 		'manage_options',
 		'wds-headless-options-page',
 		'wds_headless_render_options_page'
 	);
-
 }
 
 /**
  * Renders the options page for the settings.
  *
  * @author WebDevStudios
- * @since 1.0
+ * @since  1.0.0
  */
 function wds_headless_render_options_page() {
-	include_once 'views/options-view.php';
+	require_once 'views/options-view.php';
 }
 
-add_action( 'load-tools_page_wds-headless-options-page', 'wds_headless_save_options' );
 /**
- * Validates the user's permission to save settings and updates them accordingly.
+ * Renders the partial for displaying an error if the user is unable to save.
  *
  * @author WebDevStudios
- * @since 1.0
+ * @since  1.0.0
+ */
+function wds_headless_render_error_message() {
+	require_once 'views/partials/failure.php';
+}
+
+add_action( 'admin_post', 'wds_headless_save_options' );
+/**
+ * Validates the incoming nonce value, verifies the current user has
+ * permission to save the value from the options page and saves the
+ * option to the database.
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
  */
 function wds_headless_save_options() {
-	$action = 'wds-headless-options-save';
-	$nonce  = 'wds-headless-options-save-nonce';
 
-	// TODO: Verify user can save.
+	// First, validate the nonce and verify the user as permission to save.
+	$result = 'true';
+	if ( ! ( wds_headless_options_has_valid_nonce() && current_user_can( 'manage_options' ) ) ) {
+		$result = 'false';
+	}
+
+	wds_headless_save_404_page();
+	wds_headless_save_frontend_url();
+	wds_headless_save_preview_secret();
+
+	wds_headless_render_options_redirect( $result );
+}
+
+/**
+ * TODO
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function wds_headless_save_404_page() {
+	if ( null === wp_unslash( filter_input( INPUT_POST, 'wds-headless-404-page' ) ) ) {
+		return;
+	}
+
+	update_option(
+		'wds-headless-404-page',
+		sanitize_text_field(
+			filter_input( INPUT_POST, 'wds-headless-404-page' )
+		)
+	);
+}
+
+/**
+ * TODO
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function wds_headless_save_frontend_url() {
+	if ( null === wp_unslash( filter_input( INPUT_POST, 'wds-headless-frontend-url' ) ) ) {
+		return;
+	}
+
+	update_option(
+		'wds-headless-frontend-url',
+		sanitize_text_field(
+			filter_var(
+				filter_input( INPUT_POST, 'wds-headless-frontend-url' ),
+				FILTER_SANITIZE_URL
+			)
+		)
+	);
+}
+
+/**
+ * TODO
+ *
+ * @return void
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function wds_headless_save_preview_secret() {
+	if ( null === wp_unslash( filter_input( INPUT_POST, 'wds-headless-preview-secret' ) ) ) {
+		return;
+	}
+
+	update_option(
+		'wds-headless-preview-secret',
+		sanitize_text_field(
+			filter_var(
+				filter_input( INPUT_POST, 'wds-headless-preview-secret' ),
+				FILTER_SANITIZE_STRING
+			)
+		)
+	);
+}
+
+/**
+ * TODO
+ *
+ * @return void
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function wds_headless_save_jwt_auth () {
+	if ( null === wp_unslash( filter_input( INPUT_POST, 'wds-headless-jwt-auth-key' ) ) ) {
+		return;
+	}
+
+	update_option(
+		'wds-headless-jwt-auth-key',
+		sanitize_text_field(
+			filter_var(
+				filter_input( INPUT_POST, 'wds-headless-jwt-auth-key' ),
+				FILTER_SANITIZE_STRING
+			)
+		)
+	);
+}
+
+/**
+ * Redirects the user back to the administration page will a success or failure message.
+ *
+ * @param string $result The result of the evaluation of the settings being saved.
+ *
+ * @return void
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function wds_headless_render_options_redirect( string $result ) {
+	if ( null === filter_input( INPUT_POST, '_wp_http_referer' ) ) {
+		$_POST['_wp_http_referer'] = wp_login_url();
+	}
+
+	// Sanitize the value of the $_POST collection for the Coding Standards.
+	$url = sanitize_text_field(
+		wp_unslash( filter_input( INPUT_POST, '_wp_http_referer' ) )
+	);
+
+	// Finally, redirect back to the admin page.
+	wp_safe_redirect( urldecode( add_query_arg( [ 'success' => $result ], urldecode( $url ) ) ) );
+	exit;
+}
+
+/**
+ * Verifies that the user has permission to save data associated with the settings page.
+ *
+ * @return bool True if the user has permission to save information; otherwise, false.
+ *
+ * @return void
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function wds_headless_options_has_valid_nonce() {
+	// If the nonce isn't even in the $_POST, then it's invalid.
+	if ( null === filter_input( INPUT_POST, 'wds-headless-settings-save-nonce' ) ) {
+		return false;
+	}
+
+	$field  = wp_unslash( filter_input( INPUT_POST, 'wds-headless-settings-save-nonce' ) );
+	$action = 'wds-headless-settings-save';
+
+	return wp_verify_nonce( $field, $action );
 }
--- themes/wds_headless/inc/views/options-view.php
+++ themes/wds_headless/inc/views/options-view.php
@@ -6,28 +6,63 @@
  * @package wds-headless-theme
  * @since   1.0
  */
+
+require_once 'partials/admin-notice.php';
 ?>
 
 <div class="wrap">
-	<h1><?php esc_html_e( 'Theme Settings', 'wds' ); ?></h1>
+	<h1><?php esc_html_e( get_admin_page_title() ); // phpcs:ignore ?></h1>
 
-	<div class="wrap">
-		<label for="wds-custom-not-found-page">
-			<strong><?php esc_html_e( '404 Page', 'wds' ); ?></strong>
-			<select name="wds-custom-not-found-page">
-				<option value="default"><?php esc_html_e( 'Default...', 'wds' ); ?></option>
-				<?php foreach ( get_pages() as $current_page ) : // phpcs:ignore ?>
-					<option value="<?php esc_attr( $current_page->ID ); ?>">
-						<?php echo $current_page->post_title; //phpcs:ignore ?>
-					</option>
-				<?php endforeach; ?>
-			</select>
-		</label>
-	</div>
+	<form method="post" action="<?php echo esc_html( admin_url( 'admin-post.php' ) ); ?>">
+		<p>
+			<label for="wds-headless-404-page">
+				<?php esc_html_e( '404 Page', 'wds' ); ?>
+				<select name="wds-headless-404-page" id="wds-headless-404-page">
+					<option value="default"><?php esc_html_e( 'Default...', 'wds' ); ?></option>
+					<?php foreach ( get_pages() as $current_page ) : ?>
+						<option value="<?php esc_attr_e( $current_page->ID ); ?>"
+						<?php selected( $current_page->ID, get_option( 'wds-headless-404-page' ), true ); // phpcs:ignore ?>
+						>
+							<?php esc_html_e( $current_page->post_title ); //phpcs:ignore ?>
+						</option>
+					<?php endforeach; ?>
+				</select>
+			</label>
+		</p>
 
+		<p>
+			<label for="wds-headless-frontend-url">
+				<?php esc_html_e( 'Headless Frontend URL', 'wds' ); ?>
+				<input
+					name="wds-headless-frontend-url" id="wds-headless-frontend-url"
+					type="text" value="<?php esc_attr_e( get_option( 'wds-headless-frontend-url' ) ); // phpcs:ignore ?>"
+				/>
+			</label>
+		</p>
 
-	<form action="" method="post">
-		<?php submit_button( __( 'Save', 'wds' ) ); ?>
-		<?php wp_nonce_field( 'wds-headless-options-save', 'wds-headless-options-save-nonce' ); ?>
-	</form>
+		<p>
+			<label for="wds-headless-preview-secret">
+				<?php esc_html_e( 'Headless Preview Secret', 'wds' ); ?>
+				<input
+					name="wds-headless-preview-secret" id="wds-headless-preview-secret"
+					type="text" value="<?php esc_attr_e( get_option( 'wds-headless-preview-secret' ) ); // phpcs:ignore ?>"
+				/>
+			</label>
+		</p>
+
+		<p>
+			<label for="wds-headless-jwt-auth-key">
+				<?php esc_html_e( 'Headless JWT Auth Key', 'wds' ); ?>
+				<input
+					name="wds-headless-jwt-auth-key" id="wds-headless-jwt-auth-key"
+					type="text" value="<?php esc_attr_e( get_option( 'wds-headless-jwt-auth-key' ) ); // phpcs:ignore ?>"
+				/>
+			</label>
+		</p>
+
+		<?php
+		wp_nonce_field( 'wds-headless-settings-save', 'wds-headless-settings-save-nonce' );
+		submit_button();
+		?>
+  </form>
 </div>
