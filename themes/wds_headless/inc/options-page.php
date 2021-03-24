41061860f2f0328c4cacb89815918ba7ecd0942c
Tom McFarlin <tom@tommcfarlin.com>
2021-03-24 09:56:37 -0400

    Added utility functions, Updated validation.

--- themes/wds_headless/inc/options-page.php
+++ themes/wds_headless/inc/options-page.php
@@ -7,6 +7,127 @@
  * @since   1.0
  */
 
+/*
+ * Utility Functions
+ * These functions are used to help make working with the options page easier.
+ */
+
+/**
+ * Determines if the constants are defined and, if so, disabled the option input fields
+ * except for the default 404 page.
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function wds_headless_is_disabled_field() {
+	if ( _wds_headless_has_defined_all_constants() ) {
+		esc_attr_e( 'disabled="true"', 'wds' );
+	}
+}
+
+/**
+ * Read the incoming constant definition key and, if defined, updates the associated option.
+ *
+ * @access private
+ *
+ * @param string $wds_headless_constant The PHP constant to check before saving data from the form.
+ * @param string $option_name           The name of the option to serialize the value of the constant.
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function _wds_headless_check_wp_config( string $wds_headless_constant, string $option_name ) {
+	$wds_headless_constant = strtoupper( $wds_headless_constant );
+	$constants             = _wds_headless_get_defined_constants();
+
+	return ( defined( $wds_headless_constant ) && isset( $constants['user'] ) );
+}
+
+/**
+ * Retrieves an associative array of constants defined in PHP.
+ *
+ * @access private
+ *
+ * @return array The associative array of PHP constants.
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function _wds_headless_get_defined_constants() : array {
+	return \get_defined_constants( true );
+}
+
+/**
+ * Verifies that all of the WDS Headless constants are defined. If all of them are defined,
+ * this function will return true; otherwise, it will return false ignoring any that are
+ * defined.
+ *
+ * @access private
+ *
+ * @return bool True if all options are set in the wp-config.php file.
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function _wds_headless_has_defined_all_constants() {
+	return (
+		_wds_headless_check_wp_config( 'HEADLESS_FRONTEND_URL', 'wds-headless-frontend-url' ) &&
+		_wds_headless_check_wp_config( 'WORDPRESS_PREVIEW_SECRET', 'wds-headless-preview-secret' ) &&
+		_wds_headless_check_wp_config( 'GRAPHQL_JWT_AUTH_SECRET_KEY', 'wds-headless-jwt-auth-key' )
+	);
+}
+
+/**
+ * Updates the options table with the specified options page with the value of the constant
+ * set in wp-config.php.
+ *
+ * @access private
+ *
+ * @param string $wds_headless_constant The PHP constant to check before saving data from the form.
+ * @param string $option_name           The name of the option to serialize the value of the constant.
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function _wds_headless_update_options( string $wds_headless_constant, string $option_name ) {
+	$constants = _wds_headless_get_defined_constants();
+	update_option( $option_name, $constants['user'][ strtoupper( $wds_headless_constant ) ] );
+}
+
+
+/*
+ * Option Page Settings.
+ * These functions use the WordPress API and the utility functions to populate the options
+ * and to support user interaction.
+ */
+
+add_action( 'after_setup_theme', 'wds_headless_read_wp_config' );
+/**
+ * This determines if the constants have already been set within the wp-config.php
+ * file and, if so, will populate the options accordingly.
+ *
+ * @return void
+ *
+ * @author WebDevStudios
+ * @since  1.0.0
+ */
+function wds_headless_read_wp_config() {
+	if ( ! _wds_headless_has_defined_all_constants() ) {
+		return;
+	}
+
+	// The consetants and corresponding options names supported by wp-config.php.
+	$config_values = [
+		'HEADLESS_FRONTEND_URL'       => 'wds-headless-frontend-url',
+		'WORDPRESS_PREVIEW_SECRET'    => 'wds-headless-preview-secret',
+		'GRAPHQL_JWT_AUTH_SECRET_KEY' => 'wds-headless-jwt-auth-key',
+	];
+
+	foreach ( $config_values as $constant => $option_name ) {
+		_wds_headless_update_options( $constant, $option_name );
+	}
+}
+
 add_action( 'admin_menu', 'wds_headless_options_page' );
 /**
  * Adds the plugin menu item to the 'Settings' menu in the WordPress dashboard.
@@ -20,28 +141,20 @@ function wds_headless_options_page() {
 		__( 'Theme Settings', 'wds' ),
 		'manage_options',
 		'wds-headless-options-page',
-		'wds_headless_render_options_page'
+		'_wds_headless_render_options_page'
 	);
 }
 
 /**
  * Renders the options page for the settings.
  *
- * @author WebDevStudios
- * @since  1.0.0
- */
-function wds_headless_render_options_page() {
-	require_once 'views/options-view.php';
-}
-
-/**
- * Renders the partial for displaying an error if the user is unable to save.
+ * @access private
  *
  * @author WebDevStudios
  * @since  1.0.0
  */
-function wds_headless_render_error_message() {
-	require_once 'views/partials/failure.php';
+function _wds_headless_render_options_page() {
+	require_once 'views/options-view.php';
 }
 
 add_action( 'admin_post', 'wds_headless_save_options' );
@@ -54,7 +167,6 @@ add_action( 'admin_post', 'wds_headless_save_options' );
  * @since  1.0.0
  */
 function wds_headless_save_options() {
-
 	// First, validate the nonce and verify the user as permission to save.
 	$result = 'true';
 	if ( ! ( wds_headless_options_has_valid_nonce() && current_user_can( 'manage_options' ) ) ) {
@@ -64,12 +176,13 @@ function wds_headless_save_options() {
 	wds_headless_save_404_page();
 	wds_headless_save_frontend_url();
 	wds_headless_save_preview_secret();
+	wds_headless_save_jwt_auth();
 
 	wds_headless_render_options_redirect( $result );
 }
 
 /**
- * TODO
+ * Saves the value of the 404 page as defined by the user.
  *
  * @author WebDevStudios
  * @since  1.0.0
@@ -88,7 +201,7 @@ function wds_headless_save_404_page() {
 }
 
 /**
- * TODO
+ * Saves the value of the frontend URL as defined by the user.
  *
  * @author WebDevStudios
  * @since  1.0.0
@@ -110,7 +223,7 @@ function wds_headless_save_frontend_url() {
 }
 
 /**
- * TODO
+ * Saves the value of the preview secret as defined by the user.
  *
  * @return void
  *
@@ -134,14 +247,14 @@ function wds_headless_save_preview_secret() {
 }
 
 /**
- * TODO
+ * Saves the value of the JWT auth key secret as defined by the user.
  *
  * @return void
  *
  * @author WebDevStudios
  * @since  1.0.0
  */
-function wds_headless_save_jwt_auth () {
+function wds_headless_save_jwt_auth() {
 	if ( null === wp_unslash( filter_input( INPUT_POST, 'wds-headless-jwt-auth-key' ) ) ) {
 		return;
 	}
@@ -187,13 +300,16 @@ function wds_headless_render_options_redirect( string $result ) {
  *
  * @return bool True if the user has permission to save information; otherwise, false.
  *
- * @return void
- *
  * @author WebDevStudios
  * @since  1.0.0
  */
 function wds_headless_options_has_valid_nonce() {
-	// If the nonce isn't even in the $_POST, then it's invalid.
+	// If the current user doens't have permission to save, then don't.
+	if ( ! current_user_can( 'manage_options' ) ) {
+		return false;
+	}
+
+	// If the nonce isn't even in the $_POST array, then it's invalid.
 	if ( null === filter_input( INPUT_POST, 'wds-headless-settings-save-nonce' ) ) {
 		return false;
 	}
