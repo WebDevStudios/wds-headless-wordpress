cff1da0b54d38e291e03d7f479beefa646dd0295
R A Van Epps <rave@ravanepps.com>
2021-04-28 14:16:44 -0600

    Add color hex values to paragraph block

--- themes/wds_headless/js/blocks.js
+++ themes/wds_headless/js/blocks.js
@@ -1,3 +1,7 @@
+const { validateThemeColors } = wp.blockEditor;
+const { useEffect } = wp.element;
+const { addFilter } = wp.hooks;
+
 /**
  * Additional Gutenberg block functionality.
  *
@@ -14,3 +18,82 @@ wp.domReady(() => {
 		label: "Full Width",
 	});
 });
+
+addFilter(
+	"blocks.registerBlockType",
+	"wds/filterParagraphBlockAttrs1",
+	wdsAddColorPaletteHexValues
+);
+
+/**
+ * Filter block registration to add custom color attributes to paragraph block.
+ *
+ * TODO: Extend this to apply to other blocks that use the color palette.
+ *
+ * @param {object} settings Block settings config.
+ * @param {string} name     Block name.
+ * @return {object}         Block settings config.
+ */
+function wdsAddColorPaletteHexValues(settings, name) {
+	if ("core/paragraph" !== name) {
+		return settings;
+	}
+
+	// Add hex color attributes.
+	settings.attributes = {
+		...settings.attributes,
+		backgroundColorHex: {
+			type: "string",
+			default: "",
+		},
+		textColorHex: {
+			type: "string",
+			default: "",
+		},
+	};
+
+	return {
+		...settings,
+		edit(props) {
+			const {
+				attributes: { backgroundColor, textColor },
+			} = props;
+
+			useEffect(() => {
+				// Note: This may not work as expected if a custom theme palette has been set.
+				// In that case, this filter may need to be customized.
+				const defaultColors = validateThemeColors();
+
+				// Check for presence of background color attr.
+				if (backgroundColor) {
+					// Get color object by slug.
+					const backgroundColorObj = defaultColors.filter(
+						(color) => color.slug === backgroundColor
+					);
+
+					// Retrieve color hex value.
+					props.attributes.backgroundColorHex =
+						backgroundColorObj?.[0]?.color || null;
+				} else {
+					props.attributes.backgroundColorHex = "";
+				}
+
+				// Check for presence of text color attr.
+				if (textColor) {
+					// Get color object by slug.
+					const textColorObj = defaultColors.filter(
+						(color) => color.slug === textColor
+					);
+
+					// Retrieve color hex value.
+					props.attributes.textColorHex =
+						textColorObj?.[0]?.color || null;
+				} else {
+					props.attributes.textColorHex = "";
+				}
+			}, [backgroundColor, textColor]);
+
+			return settings.edit(props);
+		},
+	};
+}
