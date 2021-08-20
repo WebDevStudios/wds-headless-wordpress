cae6e9f965a4c507b586cc335d34d6cbc26e3fc4
R A Van Epps <rave@ravanepps.com>
2021-08-20 15:01:29 -0600

    Add headless plugin main file

---
+++ plugins/wds-headless/wds-headless.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Plugin Name: WDS Headless
+ * Plugin URI: https://github.com/WebDevStudios/wds-headless-wordpress
+ * Description: This plugin supports the JAMStack-powered frontend.
+ * Author: WebDevStudios <contact@webdevstudios.com>
+ * Author URI: https://webdevstudios.com
+ * Version: 1.0.0
+ * Requires at least: 5.6
+ * Requires PHP: 7.4
+ * License: GPL-2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * @package WDS_Headless
+ */
+
+namespace WDS_Headless;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	return;
+}
