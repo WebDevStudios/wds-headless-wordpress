ee04475e68c3ea9f4e9afa059c8177b913f0e5b9
R A Van Epps <rave@ravanepps.com>
2021-08-25 11:04:00 -0600

    Add constants

--- plugins/wds-headless-core/wds-headless-core.php
+++ plugins/wds-headless-core/wds-headless-core.php
@@ -20,7 +20,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 	return;
 }
 
+// Define constants.
 define( 'WDS_HEADLESS_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+define( 'WDS_HEADLESS_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+define( 'WDS_HEADLESS_VERSION', '1.0.0' );
 
 // Load TGM Plugin Activation.
 require_once 'inc/tgm/tgm.php';
